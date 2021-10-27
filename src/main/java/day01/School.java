18ce0cd9b5a225a3002e3bdfacfa892f30b6ceec
K4t1 <kata.percze@gmail.com>
2021-10-27 14:32:11 +0200

    isHeightsIncreasing

---
+++ src/main/java/day01/School.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class School {
+    public static void main(String[] args) {
+        Students student = new Students();
+        student.addHeight(123);
+        student.addHeight(122);
+        student.addHeight(129);
+        student.addHeight(130);
+
+        System.out.println(student.isHeightsIncreasing());
+    }
+}
